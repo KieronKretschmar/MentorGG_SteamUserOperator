c528621ead56e96d97e1d46c38def5ab4b48ba32
Kieron <kieron@mentor.gg>
2020-01-08 17:27:46 +0100

    Implement tests (all pass)

--- SteamUserOperatorTests/SteamInfoRedisTests.cs
+++ SteamUserOperatorTests/SteamInfoRedisTests.cs
@@ -1,14 +1,72 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
 using SteamUserOperator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SteamUserOperatorTests
 {
     [TestClass]
     public class SteamInfoRedisTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly IConfigurationRoot config;
+
+        public SteamInfoRedisTests()
         {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            config = builder.Build();
+        }
+
+        /// <summary>
+        /// Tests SetSteamUsers and GetSteamUsers with multiple users and verifies that they work as expected.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task SetAndGetUserInfos()
+        {
+            var logMock = new Mock<ILogger<SteamInfoRedis>>().Object;
+            var redis = new SteamInfoRedis(logMock, config);
+
+            // Create user with random properties
+            // Note: The steamId does not belong to users and thus their cached data can be manipulated even 
+            // on the production redis server without real-world repercussions
+            var users = SteamUserOperatorTestHelper.GetRealTestSteamIds()
+                .Select(steamId => new SteamUser
+                {
+                    SteamId = steamId,
+                    ImageUrl = RandomString(10),
+                    SteamName = RandomString(12),
+                })
+                .ToList();
+
+            // Insert users to cache
+            await redis.SetSteamUsers(users);
+
+            // Load users from redis cache
+            var cachedUsers = await redis.GetSteamUsers(users.Select(x=>x.SteamId).ToList());
+
+            // Check if data returned from cache is identical
+            foreach (var user in users)
+            {
+                // Assert equality by comparing serialized representations
+                var cachedJson = JsonConvert.SerializeObject(cachedUsers.Single(x => x.SteamId == user.SteamId));
+                var localJson = JsonConvert.SerializeObject(user);
+                Assert.AreEqual(localJson, cachedJson);
+            }
+        }
+
+        private static string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
--- SteamUserOperatorTests/SteamUserOperatorTestHelper.cs
+++ SteamUserOperatorTests/SteamUserOperatorTestHelper.cs
@@ -16,5 +16,35 @@ namespace SteamUserOperatorTests
                 SteamName = "randomName_" + new Random().Next(1, 99999999),
             };
         }
+
+        public static List<long> GetRealSteamIds()
+        {
+            return new List<long>
+            {
+                76561198033880857,
+                76561197992897571,
+                76561197994256276,
+                76561198034944406,
+                76561197985194911,
+                76561198042587946,
+                76561198142175587,
+                76561198853642003,
+                76561198433063343,
+                76561198166019050,
+                76561198044966222,
+            };
+        }
+
+        /// <summary>
+        /// Returns list of steamaccounts that are not used by real people (e.g. dev accounts)
+        /// </summary>
+        /// <returns></returns>
+        public static List<long> GetRealTestSteamIds()
+        {
+            return new List<long>
+            {
+                76561199011630801,
+            };
+        }
     }
 }
