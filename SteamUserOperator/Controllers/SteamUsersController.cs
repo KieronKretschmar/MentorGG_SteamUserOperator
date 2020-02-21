ff89dd34949c81ecf2df0d2a4119a98ba1fb53be
Kieron <kieron@mentor.gg>
2020-02-21 15:33:33 +0100

    Remove /api from route

--- SteamUserOperator/Controllers/SteamUsersController.cs
+++ SteamUserOperator/Controllers/SteamUsersController.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace SteamUserOperator.Controllers
 {
-    [Route("api/users")]
+    [Route("users")]
     [ApiController]
     public class SteamUsersController : ControllerBase
     {
